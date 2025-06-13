a545fe99861990a5ffe1b18faf59a344edcc620c
PabloGularte <pablo.gularte@proton.me>
2025-06-13 09:48:39 -0300

    Más correcciones del profe

--- src/main/resources/sql/DDL_escuela.sql
+++ src/main/resources/sql/DDL_escuela.sql
@@ -10,11 +10,11 @@ USE escuela ;
 DROP TABLE IF EXISTS grados ;
 
 CREATE TABLE IF NOT EXISTS grados (
-  id_grado INT NOT NULL AUTO_INCREMENT,
+  id_grado INT AUTO_INCREMENT,
   nombre_grado ENUM('Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto', 'Sexto', 'Séptimo') NOT NULL DEFAULT 'Primero',
   ciclo ENUM('Primer ciclo', 'Segundo ciclo') NOT NULL DEFAULT 'Primer ciclo',
-  turno ENUM('Mañana', 'Tarde', 'Jornada completa') NULL,
-  docente VARCHAR(100) NULL,
+  turno ENUM('Mañana', 'Tarde', 'Jornada completa') NOT NULL,
+  docente VARCHAR(100),
   activo BOOLEAN NOT NULL DEFAULT 1,
   PRIMARY KEY (id_grado))
 ;
@@ -26,14 +26,14 @@ CREATE TABLE IF NOT EXISTS grados (
 DROP TABLE IF EXISTS estudiantes ;
 
 CREATE TABLE IF NOT EXISTS estudiantes (
-  id_estudiante INT NOT NULL AUTO_INCREMENT,
-  id_grado INT NULL,
+  id_estudiante INT AUTO_INCREMENT,
+  id_grado INT,
   nombre VARCHAR(45) NOT NULL,
   apellido VARCHAR(45) NOT NULL,
   edad INT NOT NULL DEFAULT 6,
   direccion VARCHAR(200) NOT NULL,
-  nombre_madre VARCHAR(100) NULL,
-  nombre_padre VARCHAR(100) NULL,
+  nombre_madre VARCHAR(100),
+  nombre_padre VARCHAR(100),
   hermano_en_escuela BOOLEAN NOT NULL DEFAULT 0,
   activo BOOLEAN NOT NULL DEFAULT 1,
   PRIMARY KEY (id_estudiante));
@@ -50,9 +50,9 @@ ADD CONSTRAINT fk_estudiantes_grados
 DROP TABLE IF EXISTS asistencias ;
 
 CREATE TABLE IF NOT EXISTS asistencias (
-  id_asistencia INT NOT NULL AUTO_INCREMENT,
-  fecha DATE NULL,
-  id_estudiante INT NULL,
+  id_asistencia INT AUTO_INCREMENT,
+  fecha DATE NOT NULL,
+  id_estudiante INT NOT NULL,
   tipo_asistencia ENUM('Presente', 'Ausente', 'Llegada tarde') NOT NULL DEFAULT 'Presente',
   PRIMARY KEY (id_asistencia));
 
@@ -68,9 +68,9 @@ ADD CONSTRAINT fk_asistencias_estudiantes
 DROP TABLE IF EXISTS asignaturas ;
 
 CREATE TABLE IF NOT EXISTS asignaturas (
-  id_asignatura INT NOT NULL AUTO_INCREMENT,
-  nombre_asignatura VARCHAR(45) NULL,
-  docente VARCHAR(45) NULL,
+  id_asignatura INT AUTO_INCREMENT,
+  nombre_asignatura VARCHAR(45),
+  docente VARCHAR(45),
   PRIMARY KEY (id_asignatura));
 
 
@@ -80,12 +80,12 @@ CREATE TABLE IF NOT EXISTS asignaturas (
 DROP TABLE IF EXISTS notas ;
 
 CREATE TABLE IF NOT EXISTS notas (
-  id_nota INT NOT NULL AUTO_INCREMENT,
-  nota INT NULL,
-  anio INT NULL,
+  id_nota INT AUTO_INCREMENT,
+  nota INT NOT NULL,
+  anio INT NOT NULL,
   bimestre ENUM('Primer bimestre', 'Segundo bimestre', 'Tercer bimestre', 'Cuarto bimestre') NOT NULL DEFAULT 'Primer bimestre',
-  id_estudiante INT NULL,
-  id_asignatura INT NULL,
+  id_estudiante INT NOT NULL,
+  id_asignatura INT NOT NULL,
   PRIMARY KEY (id_nota));
 
 ALTER TABLE notas
